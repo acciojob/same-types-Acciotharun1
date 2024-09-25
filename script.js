765ca9cb7254dfaaf6232dcabd35a548fac5109b
Acciotharun1 <166792506+Acciotharun1@users.noreply.github.com>
2024-09-25 10:22:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 function isSameType(value1, value2) {
-  // Check if both values are NaN
   if (Object.is(value1, NaN) && Object.is(value2, NaN)) {
     return true;
   } else if (typeof value1 === typeof value2){
-	  return true;
-  } else{
-	  return false;
+    return true;
+  } else {
+    return false;
   }
 }
 alert(isSameType(NaN, NaN));
